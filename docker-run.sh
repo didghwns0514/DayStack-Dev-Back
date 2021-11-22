c95396f32193394369ee939705cf6604185dfa33
HoJun Yang <didghwns0514@gmail.com>
2021-11-22 20:20:49 +0900

    config(docker-run.sh):Update link for DB

--- docker-run.sh
+++ docker-run.sh
@@ -20,7 +20,7 @@ echo "Current files ls : "
 ls
 #docker run --name DayStack-Spring-Server -d -p 5551:8080 korshika/daystack-back-spring:latest
 #docker run --name DayStack-Spring-Server -d -p 5551:8080 -v /build/libs:/deploy korshika/daystack-back-spring:latest
-docker run --network shareNetwork-daystack --name DayStack-Spring-Server -d -p 5551:8080 --link mysql-daystack:mysql-daystack korshika/daystack-back-spring:latest
+docker run --network shareNetwork-daystack --name DayStack-Spring-Server -d -p 5551:8080 --link mysql-daystack korshika/daystack-back-spring:latest
 
 
 echo ""
